bdb1c1a51ce781cc52274869849cc7fc43281195
mattr <matt.roland1029@gmail.com>
2025-02-04 00:04:53 -0600

    Add cmdline preparations

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NULLIFY Docker Introduction Presentation
 by Matthew Roland
 
-Uhhh... More stuff will be here I promise
+You will need Rust installed to be able to run this program. Not that it does much now, but it will some day. You just need to believe
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::command;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cmdline().get_matches();
+
+    match matches.subcommand() {
+        _ => {
+            println!("Yay! You're playing tictactoe right now!!! In your mind!!!!!")
+        }
+    }
+}
+
+fn cmdline() -> clap::Command {
+    command!()
+        .about("A little tictactoe game to demonstrate docker")
+    // btw, if there is no ; at the end of a line in rust it implicitly returns
 }
